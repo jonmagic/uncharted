ae23d1785c0dd67f1f65b13856dbfe0453a4507e
Jonathan Hoyt <hoyt@github.com>
2013-06-02 14:47:51 -0700

    Require bloops in uncharted.

--- test/lib/test_uncharted.rb
+++ test/lib/test_uncharted.rb
@@ -5,4 +5,8 @@ class TestUncharted < MiniTest::Unit::TestCase
   def test_uncharted_plays
     assert_equal "play", Uncharted.play
   end
+
+  def test_uncharted_synth_is_bloops
+    assert Uncharted.synth.is_a?(Bloops)
+  end
 end
